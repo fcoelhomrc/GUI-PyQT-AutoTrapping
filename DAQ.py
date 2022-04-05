edabf089702bdf31b13ae51ac8ab9dd87cecfb22
CarlosVivingz <jmfoliveira110@gmail.com>
2022-04-05 14:58:06 +0100

    Changes and addition of DAQ.py

---
+++ DAQ.py
@@ -0,0 +1,23 @@
+import nidaqmx
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+class DAQ():
+    
+    def __init__(self):
+        super().__init__()
+         
+        self.task = nidaqmx.Task()
+        #device = "Dev1"
+        self.device = 'OTKB' #requires initialization in NI MAX
+        self.task.ai_channels.add_ai_voltage_chan(self.device + "/ai0")
+        self.task.ai_channels.add_ai_voltage_chan(self.device + "/ai1")
+        self.task.ai_channels.add_ai_voltage_chan(self.device + "/ai2")
+    
+    def start(self):
+        self.task.start()
+        
+    def stop(self):
+        self.task.stop()
+
